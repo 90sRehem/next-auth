1160a4b0ec5edf194dc308edf420a9a635f47180
Jonathan Rehem <jonathan.de.oliveira@live.com>
2021-05-16 17:32:48 -0300

    broadcasting

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,18 +6,20 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-    const { user } = useContext(AuthContext)
+    const { user, signOut } = useContext(AuthContext)
 
     useEffect(() => {
         api.get('/me')
             .then(response => console.log(response))
             .catch(error => console.log(error))
-
     }, []);
 
     return (
         <>
             <h1>Dashboard: {user?.email}</h1>
+
+            <button onClick={signOut}>SignOut</button>
+
             <Can permissions={['metrics.list']}>
                 <div>Métricas</div>
             </Can>
